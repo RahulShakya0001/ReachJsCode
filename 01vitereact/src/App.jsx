e84ecb1a1ba343198ad298b6decf531e52a5c962
LoginPage <rahulshakya7685@gmail.com>
2025-04-16 16:46:24 +0530

    Add Chai component and update App structure; implement custom render logic

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,9 +1,23 @@
 import { useState } from 'react'
+import Chai from "./chai"
+
+// const reactElement = {
+//   type: "a",
+//   props: {
+//       href: "http://google.com",
+//       target: "_black"
+//   },
+//   children: "Click me to visit google"
+// }
+
 
 function App() {
-
+  const username = "Rahul Saini";
   return (
-    <h1>Chai Aur React With Vite | RK</h1>
+    <>
+    <h1>Chai Aur Code With Username : {username}</h1>
+    <p>Tera para</p>
+    </>
   )
 }
 
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,9 +1,32 @@
-import { StrictMode } from 'react'
+import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
+
+function MyApp(){
+  return  (
+    <div>
+      <h1>Custom App</h1>
+    </div>
+  )
+}
+// const reactElement = {
+//   type: "a",
+//   props: {
+//       href: "http://google.com",
+//       target: "_black"
+//   },
+//   children: "Click me to visit google"
+// }
+const anotherUser = "Chai aur REact";
+const reactElement = React.createElement(
+  "a",
+  {href: "https://google.com", target: "_blank"},
+  "click me to Visit Google",
+  anotherUser
+)
+
+
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  reactElement
 )
